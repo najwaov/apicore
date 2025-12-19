30eeedca148b1e8aab4011dd15d1ea5aa053d43d
najwaov <163403277+najwaov@users.noreply.github.com>
2025-12-19 13:37:34 +0530

    add one component

---
+++ src/app/emp/emp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp',
+  imports: [],
+  templateUrl: './emp.component.html',
+  styleUrl: './emp.component.css'
+})
+export class EmpComponent {
+
+}
